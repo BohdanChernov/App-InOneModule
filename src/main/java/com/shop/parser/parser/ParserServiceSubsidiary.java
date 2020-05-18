1522559d003076517b237443b8df494ec5777cb4
BohdanChernov <b.chernov.kiev@gmail.com>
2020-05-18 13:31:37 +0300

    Small change.

--- src/main/java/com/shop/parser/parser/ParserServiceSubsidiary.java
+++ src/main/java/com/shop/parser/parser/ParserServiceSubsidiary.java
@@ -186,9 +186,9 @@ public class ParserServiceSubsidiary {
 //        String GOOGLE_CHROME_BIN = "/app/.apt/opt/google/chrome/google-chrome";
 
 //        options.setBinary(GOOGLE_CHROME_BIN);
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--headless");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
         WebDriver driver = new ChromeDriver(options);
 
         // END OF TEST CODE
