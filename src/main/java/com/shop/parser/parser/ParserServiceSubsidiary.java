b7d9396aaa0c01e410afaea43a1bbb8e342a69f2
BohdanChernov <b.chernov.kiev@gmail.com>
2020-05-18 15:52:33 +0300

    Small change.

--- src/main/java/com/shop/parser/parser/ParserServiceSubsidiary.java
+++ src/main/java/com/shop/parser/parser/ParserServiceSubsidiary.java
@@ -130,6 +130,9 @@ public class ParserServiceSubsidiary {
             processorType = matcher3.group(0).trim();
         }
 
+
+        System.out.println(processor);
+        System.out.println(processor.text());
         System.out.println("PROCESSOR: " + processorType);
         return processorType;
     }
